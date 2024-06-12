0fd083e3655f51c5a86d252b66431bb30bb7bc01
Auggustto <leonardoaugusto1000a@gmail.com>
2024-06-12 18:23:39 -0300

    Error while creating migrations: Can't locate revision identified by 'a96c02ec78c7'

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,39 @@
 FROM python:3.11-slim-buster
 
+# Definir variáveis de ambiente
 ENV PYTHONUNBUFFERED 1
 ENV PATH="/root/.local/bin:$PATH"
 ENV PYTHONPATH="/"
 
-COPY ./poetry.lock /
-COPY ./pyproject.toml /
+# Instalar dependências do sistema
+RUN apt-get update && apt-get install -y curl gcc python3-dev libpq-dev && apt-get clean
 
-#  Instalando o cur para instalar o poetry
-RUN apt-get update -y && apt-get install curl -y \
-&& curl -sSL https://install.python-poetry.org | python3 - \
-#  Configuração para não criar o env
-&& poetry config virtualenvs.create false\
-&& poetry install \
-#  Remover o curl
-&& apt-get remove curl -y
+# Instalar Poetry
+RUN curl -sSL https://install.python-poetry.org | python3 -
 
-COPY . .
+# Configurar Poetry para não criar ambientes virtuais
+RUN poetry config virtualenvs.create false
+
+# Copiar arquivos do Poetry
+COPY pyproject.toml poetry.lock /
+
+# Instalar dependências do projeto
+RUN poetry install --no-root --no-dev
+
+# Remover pacotes desnecessários
+RUN apt-get remove -y curl && apt-get autoremove -y && apt-get clean
+
+# Criar diretório de migrações e ajustar permissões para todos os usuários
+RUN mkdir -p /app/migrations/versions && chmod -R 777 /app/migrations/versions
+
+# Copiar código do aplicativo
+COPY . /app
+
+# Definir diretório de trabalho
 WORKDIR /app
 
-# expor a porta 8000 
+# Expor a porta 8000
 EXPOSE 8000
 
+# Comando para iniciar o aplicativo
 CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- app/migrations/env.py
+++ app/migrations/env.py
@@ -1,50 +1,40 @@
 from logging.config import fileConfig
-
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
+import os
+from sqlalchemy import engine_from_config, pool
 from alembic import context
-from decouple import config as decouple_config
-from app.models.models import Base
+from app.models.base import Base
+from app.models.connection import engine  # Importe o objeto de engine do SQLAlchemy
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
+# Este é o objeto de configuração do Alembic, que fornece acesso aos valores dentro do arquivo .ini em uso.
 config = context.config
-DB_URL = decouple_config('DB_URL')
-config.set_main_option("sqlalchemy.url", DB_URL)
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
+# Interpretar o arquivo de configuração para Python logging.
+# Esta linha configura basicamente os loggers.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
+# Importar o objeto MetaData dos modelos
 target_metadata = Base.metadata
 
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
+# Recuperar a URL do banco de dados a partir da variável de ambiente
+database_url = os.getenv('DB_URL')
 
+if database_url is None:
+    raise EnvironmentError("DATABASE_URL environment variable not set")
+
+# Configurar a URL do SQLAlchemy no objeto de configuração do Alembic
+config.set_main_option('sqlalchemy.url', database_url)
 
 def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode.
+    """Executar migrações no modo 'offline'.
 
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
+    Isso configura o contexto apenas com uma URL e não um Engine, embora um Engine também seja aceitável aqui. 
+    Ao pular a criação do Engine, nem precisamos de um DBAPI disponível.
 
-    Calls to context.execute() here emit the given string to the
-    script output.
-
+    As chamadas para context.execute() aqui emitem a string dada para a saída do script.
     """
-    url = config.get_main_option("sqlalchemy.url")
     context.configure(
-        url=url,
+        url=database_url,  # Use a URL do banco de dados
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
@@ -55,17 +45,11 @@ def run_migrations_offline() -> None:
 
 
 def run_migrations_online() -> None:
-    """Run migrations in 'online' mode.
+    """Executar migrações no modo 'online'.
 
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-
+    Nesse cenário, precisamos criar um Engine e associar uma conexão com o contexto.
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    connectable = engine  # Use o objeto de engine do SQLAlchemy
 
     with connectable.connect() as connection:
         context.configure(
