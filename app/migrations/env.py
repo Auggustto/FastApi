16a57b80d56f438fe8e5b9436629c792c422e7fb
Auggustto <leonardoaugusto1000a@gmail.com>
2024-06-13 11:27:02 -0300

    Resolution of the error creating migrations. The entire migration schema was recreated, which made it possible to detach the migration file that was causing the error.

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -1,40 +1,58 @@
 from logging.config import fileConfig
-import os
-from sqlalchemy import engine_from_config, pool
-from alembic import context
-from app.models.base import Base
-from app.models.connection import engine  # Importe o objeto de engine do SQLAlchemy
 
-# Este é o objeto de configuração do Alembic, que fornece acesso aos valores dentro do arquivo .ini em uso.
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
+
+from alembic import context
+
+# pegando os valores do env
+from decouple import config as config_decouple
+
+# Importando o Base para geração automatica das migrações
+from app.models.models import Base
+
+
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
 config = context.config
 
-# Interpretar o arquivo de configuração para Python logging.
-# Esta linha configura basicamente os loggers.
+#  Configurando o sqlalchemy
+DB_URL = config_decouple('DB_URL')
+#  Definindo a url do sqlalchemy.url dinamicamente
+config.set_main_option('sqlalchemy.url', DB_URL)
+
+# Interpret the config file for Python logging.
+# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# Importar o objeto MetaData dos modelos
+# add your model's MetaData object here
+# for 'autogenerate' support
+# from myapp import mymodel
+# target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
 
-# Recuperar a URL do banco de dados a partir da variável de ambiente
-database_url = os.getenv('DB_URL')
+# other values from the config, defined by the needs of env.py,
+# can be acquired:
+# my_important_option = config.get_main_option("my_important_option")
+# ... etc.
 
-if database_url is None:
-    raise EnvironmentError("DATABASE_URL environment variable not set")
-
-# Configurar a URL do SQLAlchemy no objeto de configuração do Alembic
-config.set_main_option('sqlalchemy.url', database_url)
 
 def run_migrations_offline() -> None:
-    """Executar migrações no modo 'offline'.
+    """Run migrations in 'offline' mode.
 
-    Isso configura o contexto apenas com uma URL e não um Engine, embora um Engine também seja aceitável aqui. 
-    Ao pular a criação do Engine, nem precisamos de um DBAPI disponível.
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
 
-    As chamadas para context.execute() aqui emitem a string dada para a saída do script.
+    Calls to context.execute() here emit the given string to the
+    script output.
+
     """
+    url = config.get_main_option("sqlalchemy.url")
     context.configure(
-        url=database_url,  # Use a URL do banco de dados
+        url=url,
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
@@ -45,11 +63,17 @@ def run_migrations_offline() -> None:
 
 
 def run_migrations_online() -> None:
-    """Executar migrações no modo 'online'.
+    """Run migrations in 'online' mode.
 
-    Nesse cenário, precisamos criar um Engine e associar uma conexão com o contexto.
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
+
     """
-    connectable = engine  # Use o objeto de engine do SQLAlchemy
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section, {}),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
 
     with connectable.connect() as connection:
         context.configure(
